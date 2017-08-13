7fcf1c94e46fa5ab31e6c7ac371abff8272a247e
Goku <Goku@DESKTOP-0C7QAVJ>
2017-08-12 19:02:20 -0600

    tareaPracticaUno_199919737

---
+++ src/pruebacal/ClaseEventos.java
@@ -0,0 +1,30 @@
+
+package pruebacal;
+
+import com.sun.glass.events.KeyEvent;
+import java.awt.RenderingHints.Key;
+import java.awt.event.KeyListener;
+
+
+public class ClaseEventos implements KeyListener {
+
+   
+    public void keyTyped(java.awt.event.KeyEvent e) {
+        
+    }
+
+   
+    public void keyPressed(java.awt.event.KeyEvent e) {
+       
+    }
+
+    public void keyReleased(java.awt.event.KeyEvent e) {
+        
+    }
+    
+}
+
+
+
+    
+
