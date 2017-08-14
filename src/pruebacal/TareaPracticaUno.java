717cba47179362889a6505b79b8eb3719627699a
Goku <Goku@DESKTOP-0C7QAVJ>
2017-08-14 14:05:17 -0600

    cuartocomit

--- src/pruebacal/TareaPracticaUno.java
+++ src/pruebacal/TareaPracticaUno.java
@@ -22,8 +22,24 @@ public class TareaPracticaUno {
         
         switch(opciòn){
             case 1:
-                ir.inicio();
-                break;
+                PresentaciónDeIngreso ha = new PresentaciónDeIngreso();
+                
+        
+            java.awt.event.KeyEvent e = null;
+            ha.keyTyped(e);
+        
+                     System.out.println("Nombre");
+               System.out.println("Carné");
+               System.out.println("IPC"+"'A'"+"Practica1");
+               System.out.println("BUSCAMINAS!!!!");
+            
+     Menu2 muestra = new Menu2();
+         char presioneEnter = e.getKeyChar();
+         if(presioneEnter == KeyEvent.VK_ENTER){
+             muestra.menu2(); 
+         }
+              break;
+         
             case 2:
                 ver.menu2();
                 break;
@@ -32,6 +48,9 @@ public class TareaPracticaUno {
     }//fin del while
 }//fin del método menuPresentaci+on
  public static void main(String[] args){
+     PresentaciónDeIngreso mirar = new PresentaciónDeIngreso();
+     
+     
     TareaPracticaUno entrar = new TareaPracticaUno();
     entrar.menuPresentacion();
         
